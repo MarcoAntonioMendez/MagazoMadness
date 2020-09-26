154a2fd68517bc4bf648e9c798f267870f6d85a5
Marco Méndez <marco.mendez.ing@gmail.com>
2020-09-26 00:34:37 -0500

    Set app background and hid the Android control buttons

--- app/src/main/java/com/zapatoseducadosgames/magazomadness/MainActivity.java
+++ app/src/main/java/com/zapatoseducadosgames/magazomadness/MainActivity.java
@@ -8,12 +8,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity{
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.relative_layout);
+
+        // Setting the background image
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
+
+        hideSystemUI();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {// Windows has focus
+            hideSystemUI();
+
+        }else{// Windows does not have focus
+
+        }
+    }
+
+    // Hides the systemsUI
+    private void hideSystemUI(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
 }
