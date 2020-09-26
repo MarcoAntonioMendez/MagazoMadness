1a79b27f89cdca48e4df28b7dc4d3bf72c657462
Marco Méndez <marco.mendez.ing@gmail.com>
2020-09-26 15:00:18 -0500

    Added the highestScoreTextView with no style, just the structure

--- app/src/main/java/com/zapatoseducadosgames/magazomadness/MainActivity.java
+++ app/src/main/java/com/zapatoseducadosgames/magazomadness/MainActivity.java
@@ -59,6 +59,17 @@ public class MainActivity extends AppCompatActivity{
 
         String highestScore = FilesHandler.readFile(AppConstants.HIGHEST_SCORE_FILE_NAME,this);
 
+        if(highestScore.isEmpty()){
+            // This means the user is playing for the first time, thus we need to create the file
+            FilesHandler.writeFile(AppConstants.HIGHEST_SCORE_FILE_NAME,"0",this);
+        }
+
+        highestScore = FilesHandler.readFile(AppConstants.HIGHEST_SCORE_FILE_NAME,this);
+        highestScoreTextView.setText(highestScore);
+        highestScoreTextView.measure(0,0);
+        highestScoreTextView.setX((screenWidth/2)-(highestScoreTextView.getMeasuredWidth()/2));
+        highestScoreTextView.setY((screenHeight/2)+highestScoreTextView.getMeasuredHeight());
+
         layout.addView(highestScoreTextView);
     }
 
