ec7762a28529f599505b5446997cff6803e54fc4
Marco Méndez <marco.mendez.ing@gmail.com>
2020-09-26 00:45:58 -0500

    Added code chunk to know width and height of Android Device

--- app/src/main/java/com/zapatoseducadosgames/magazomadness/MainActivity.java
+++ app/src/main/java/com/zapatoseducadosgames/magazomadness/MainActivity.java
@@ -1,10 +1,12 @@
 package com.zapatoseducadosgames.magazomadness;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,15 +14,23 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity{
     private RelativeLayout layout;
+    private int screenWidth,screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         layout = findViewById(R.id.relative_layout);
 
         // Setting the background image
         getWindow().setBackgroundDrawableResource(R.drawable.background);
+
+        // Getting the screen's resolution of the current device
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
 
         hideSystemUI();
     }
